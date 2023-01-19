6b4738cbdc8a1d26f9ad3da3c44b1f068bccf84a
rmenon1008 <rmenon1008@gmail.com>
2023-01-18 21:11:58 -0800

    Added IP address location

--- index.js
+++ index.js
@@ -6,18 +6,15 @@ var app = express();
 app.set('view engine', 'hbs');
 app.use(cors());
 
-// Weather API Proxy
-const API_KEY = '046da01711fa493ea63214619220602';
-app.get("/weather/:ip", (req, res, next) => {
+const PIRATE_API_KEY = 'FVnljLZ86C6OP4bHRvsEC2HpHY2bHBku19EPD2vD';
+const ABSTRACT_API_KEY = '520e9572a104429d96d7bd8e926a734b';
+
+app.get("/weather/:loc", (req, res, next) => {
     try {
-        var ip = req.params.ip;
-        var queryURL = 'http://api.weatherapi.com/v1/forecast.json?key=' + API_KEY + '&q=' + ip + '&days=2';
+        const loc = req.params.loc;
+        const queryURL = `https://api.pirateweather.net/forecast/${PIRATE_API_KEY}/${loc}`;
         fetch(queryURL).then(data => {
             data.json().then(json => {
-                var nextDay = json.forecast.forecastday[1].hour;
-                var today = json.forecast.forecastday[0].hour.concat(nextDay);
-                json.forecast.forecastday[0].hour = today;
-                delete json.forecast.forecastday[1];
                 res.json(json);
             });
         });
@@ -29,14 +26,20 @@ app.get("/weather/:ip", (req, res, next) => {
 app.get("/weather", (req, res, next) => {
     try {
         const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-        var queryURL = 'http://api.weatherapi.com/v1/forecast.json?key=' + API_KEY + '&q=' + ip + '&days=2';
-        fetch(queryURL).then(data => {
+        // get the lat and long of the ip
+        const geolocationQueryURL = `https://ipgeolocation.abstractapi.com/v1/?api_key=${ABSTRACT_API_KEY}&ip_address=${ip}`;
+        fetch(geolocationQueryURL).then(data => {
             data.json().then(json => {
-                var nextDay = json.forecast.forecastday[1].hour;
-                var today = json.forecast.forecastday[0].hour.concat(nextDay);
-                json.forecast.forecastday[0].hour = today;
-                delete json.forecast.forecastday[1];
-                res.json(json);
+                const lat = json.latitude;
+                const long = json.longitude;
+                
+                // get the weather of the lat and long
+                const weatherQueryURL = `https://api.pirateweather.net/forecast/${PIRATE_API_KEY}/${lat},${long}`;
+                fetch(weatherQueryURL).then(data => {
+                    data.json().then(json => {
+                        res.json(json);
+                    });
+                });
             });
         });
     } catch (error) {
@@ -48,4 +51,5 @@ app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
 
-module.exports = app;
+// module.exports = app;
+export default app;
