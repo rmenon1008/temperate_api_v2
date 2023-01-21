00676a1993c8aa6b00b630bae154181b88a87ed6
rmenon1008 <rmenon1008@gmail.com>
2023-01-21 15:22:02 -0800

    Removes debug print

--- getImage.js
+++ getImage.js
@@ -66,7 +66,6 @@ const updateSingleCollection = async (collection) => {
 
 const getBestColors = async (url) => {
     const candidateColors = addColorVariants(await colorThief.getPaletteFromURL(url, 5));
-    console.log(candidateColors);
     const pixels = await getPixelArrayFromURL(url, startRow=5, endRow=Infinity, startCol=0, endCol=9);
 
     let colorContrastsMain = {};
