48012411e95d31951ce1f27dea9feac1aefb7b02
rmenon1008 <rmenon1008@gmail.com>
2023-01-21 15:59:20 -0800

    Adds path dependency

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const fetch = require('node-fetch-commonjs');
+const path = require('path');
 
 const getWeather = require('./getWeather');
 const { updateAllCollections, updateSingleCollection, imageCollections } = require('./getImage');
